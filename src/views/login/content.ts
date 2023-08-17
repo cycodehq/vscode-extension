7b3407ec7c8c68fae98579a47f0f3de75160fb4d
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-08-17 22:49:09 +0200

    Fix using of constant color for buttons on views

--- src/views/login/content.ts
+++ src/views/login/content.ts
@@ -11,7 +11,7 @@ export default `
       flex-direction: column;
     }
     #authenticate-button {
-      background-color: #2376e5;
+      background-color: var(--vscode-button-background);
       color: white;
       outline: none;
       border: none;
