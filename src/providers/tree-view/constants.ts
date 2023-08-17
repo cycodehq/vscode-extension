bd69d1c7525d4af0ae20a89e7fac77d5ee0b8bdf
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-08-17 13:42:15 +0200

    fix path traversal

--- src/providers/tree-view/constants.ts
+++ src/providers/tree-view/constants.ts
@@ -7,9 +7,24 @@ const _PATH_TO_RESOURCES = path.join(__filename, '..', '..', 'resources');
 const PATH_TO_SCAN_TYPE_ICONS= path.join(_PATH_TO_RESOURCES, 'scan-type');
 const PATH_TO_SEVERITY_ICONS= path.join(_PATH_TO_RESOURCES, 'severity');
 
-const getScanTypeIconPath = (scanType: string) => path.join(PATH_TO_SCAN_TYPE_ICONS, `${scanType}.png`);
+const getScanTypeIconPath = (scanType: string) => {
+  _validateIconFilename(scanType);
+  return path.join(PATH_TO_SCAN_TYPE_ICONS, `${scanType}.png`);
+};
 
-export const getSeverityIconPath = (severityFirstLetter: string) => path.join(PATH_TO_SEVERITY_ICONS, `${severityFirstLetter}.png`);
+
+export const getSeverityIconPath = (severityFirstLetter: string) => {
+  _validateIconFilename(severityFirstLetter);
+  return path.join(PATH_TO_SEVERITY_ICONS, `${severityFirstLetter}.png`);
+};
+
+
+const _validateIconFilename = (filename: string): void => {
+  const letters = /^[A-Za-z]+$/;
+  if (filename.match(letters) === null) {
+    throw Error("Malformed filename string");
+  }
+};
 
 
 const SECRETS_SECTION = new TreeViewItem(
