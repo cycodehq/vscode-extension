c1aaa9d2ed771f9d740682219131c2edb6757d77
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-08-17 13:30:51 +0200

    Scan types and severity icons in the tree view

--- src/providers/tree-view/constants.ts
+++ src/providers/tree-view/constants.ts
@@ -1,12 +1,23 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 import { TreeViewItem } from './item';
 import { ScanType } from '../../constants';
+
+const _PATH_TO_RESOURCES = path.join(__filename, '..', '..', 'resources');
+const PATH_TO_SCAN_TYPE_ICONS= path.join(_PATH_TO_RESOURCES, 'scan-type');
+const PATH_TO_SEVERITY_ICONS= path.join(_PATH_TO_RESOURCES, 'severity');
+
+const getScanTypeIconPath = (scanType: string) => path.join(PATH_TO_SCAN_TYPE_ICONS, `${scanType}.png`);
+
+export const getSeverityIconPath = (severityFirstLetter: string) => path.join(PATH_TO_SEVERITY_ICONS, `${severityFirstLetter}.png`);
+
 
 const SECRETS_SECTION = new TreeViewItem(
   "Hardcoded Secrets",
   vscode.TreeItemCollapsibleState.Collapsed,
   undefined,
   ScanType.Secrets,
+  getScanTypeIconPath(ScanType.Secrets),
 );
 
 const SCA_SECTION = new TreeViewItem(
@@ -14,6 +25,7 @@ const SCA_SECTION = new TreeViewItem(
   vscode.TreeItemCollapsibleState.Collapsed,
   undefined,
   ScanType.Sca,
+  getScanTypeIconPath(ScanType.Sca),
 );
 
 const SAST_SECTION = new TreeViewItem(
@@ -21,7 +33,7 @@ const SAST_SECTION = new TreeViewItem(
   vscode.TreeItemCollapsibleState.Collapsed,
   undefined,
   ScanType.Sast,
-  undefined,
+  getScanTypeIconPath(ScanType.Sast),
   "(coming soon)",
 );
 
@@ -30,7 +42,7 @@ const IAC_SECTION = new TreeViewItem(
   vscode.TreeItemCollapsibleState.Collapsed,
   undefined,
   ScanType.Iac,
-  undefined,
+  getScanTypeIconPath(ScanType.Iac),
   "(coming soon)",
 );
 
