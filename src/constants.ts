9228450f3c73c25b14042db8cd7d84558b115eee
Ilya Siamionau <ilya.siamionau@cycode.com>
2023-08-22 13:13:15 +0200

    CM-26332 - Add remediation note for package lock files

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,13 @@
-// Source: https://github.com/cycodehq-public/cycode-cli/blob/ec8333707ab2590518fd0f36454c8636ccbf1061/cycode/cli/consts.py#L50-L82
-export const SCA_CONFIGURATION_SCAN_SUPPORTED_FILES: ReadonlyArray<string> = [
+// keep in lowercase.
+// source: https://github.com/cycodehq-public/cycode-cli/blob/ec8333707ab2590518fd0f36454c8636ccbf1061/cycode/cli/consts.py#L50-L82
+const _SCA_CONFIGURATION_SCAN_SUPPORTED_FILES: ReadonlyArray<string> = [
   'cargo.lock',
   'cargo.toml',
   'composer.json',
   'composer.lock',
   'go.sum',
   'go.mod',
+  // 'gopkg.toml', // FIXME(MarshalX): missed in CLI?
   'gopkg.lock',
   'pom.xml',
   'build.gradle',
@@ -32,6 +34,44 @@ export const SCA_CONFIGURATION_SCAN_SUPPORTED_FILES: ReadonlyArray<string> = [
   'requirements.txt',
   'setup.py',
 ];
+
+// keep in lowercase. based on _SCA_CONFIGURATION_SCAN_SUPPORTED_FILES
+const _SCA_CONFIGURATION_SCAN_LOCK_FILE_TO_PACKAGE_FILE: { [key: string]: string } = {
+  'cargo.lock': 'cargo.toml',
+  'composer.lock': 'composer.json',
+  'go.sum': 'go.mod',
+  'gopkg.lock': 'gopkg.toml',
+  'gradle.lockfile': 'build.gradle',
+  'package-lock.json': 'package.json',
+  'yarn.lock': 'package.json',
+  'packages.lock.json': 'nuget.config',
+  'gemfile.lock': 'gemfile',
+  'build.sbt.lock': 'build.sbt', // and build.scala?
+  'poetry.lock': 'pyproject.toml',
+  'pipfile.lock': 'pipfile',
+};
+
+const _SCA_CONFIGURATION_SCAN_SUPPORTED_LOCK_FILES: ReadonlyArray<string> =
+  Object.keys(_SCA_CONFIGURATION_SCAN_LOCK_FILE_TO_PACKAGE_FILE);
+
+export const isSupportedPackageFile = (fileName: string): boolean => {
+  const lowerCaseFileName = fileName.toLowerCase();
+  return _SCA_CONFIGURATION_SCAN_SUPPORTED_FILES.some(fileSuffix => lowerCaseFileName.endsWith(fileSuffix));
+};
+
+export const isSupportedLockFile = (lockFileName: string): boolean => {
+  const lowerCaseFileName = lockFileName.toLowerCase();
+  return _SCA_CONFIGURATION_SCAN_SUPPORTED_LOCK_FILES.some(fileSuffix => lowerCaseFileName.endsWith(fileSuffix));
+};
+
+export const getPackageFileForLockFile = (lockFile: string): string => {
+  const lowerCaseFileName = lockFile.toLowerCase();
+  if (!_SCA_CONFIGURATION_SCAN_LOCK_FILE_TO_PACKAGE_FILE.hasOwnProperty(lowerCaseFileName)) {
+    return 'package';
+  }
+
+  return _SCA_CONFIGURATION_SCAN_LOCK_FILE_TO_PACKAGE_FILE[lowerCaseFileName];
+};
 
 export enum ScanType {
   Secrets = "Secrets",
